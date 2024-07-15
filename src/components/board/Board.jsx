0c74eb506fa7b659a202ca0964172bbe7668e954
Ahrkhael <jjkako@gmail.com>
2024-07-15 09:33:49 +0200

    Keys separated

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -4,12 +4,13 @@ import RowBoard from "../rowBoard/RowBoard";
 
 export default function Board() {
   const rows = [];
-  for (let i = 0; i < 8; i++) {
-    rows.push(<RowBoard key={i} row={i + 1} />);
-    if((i + 1) % 8 === 0) {
-      rows.push(<br key={`br-${i}`} />);
+  for (let j = 0; j < 8; j++) {
+    rows.push(<RowBoard key={j} row={j + 1} />);
+    if((j + 1) % 8 === 0) {
+      rows.push(<br key={`br-${j}`} />);
     }
   }
+  
   return (
     <div id="board">
       {rows}
--- src/components/rowBoard/RowBoard.jsx
+++ src/components/rowBoard/RowBoard.jsx
@@ -7,6 +7,7 @@ export default function RowBoard({ row }) {
   for (let i = 0; i < 8; i++) {
     squares.push(<Square key={i} index={i + row} />);
   }
+  
   return (
     <div className="row">
       {squares}
